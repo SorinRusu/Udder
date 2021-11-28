d2ae3a6ebb35d5a20bd13b8e13cefcb06097d593
Sorin Rusu <rususorin.de@gmail.com>
2021-11-28 18:57:45 +0100

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Rusu Sorin on 28.11.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
